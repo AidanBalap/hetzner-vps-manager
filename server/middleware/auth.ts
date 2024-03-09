edfef7865a8e8b670b438b453428abb87a384fed
ElGammaYT <aidanbalap@gmail.com>
2024-03-09 18:25:08 +0100

    [fix] Middleware applying to all site

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,6 +1,7 @@
 export default defineEventHandler(async (event) => {
 
-    if (event.path.startsWith('/auth')) return;
+    if (!event.path.startsWith('/api')) return;
+    if (event.path.startsWith('/api/auth')) return;
 
     const authHeader = getCookie(event, 'auth');
 
