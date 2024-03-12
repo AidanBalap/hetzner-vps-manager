45ec538b1617b844bebd320cd08f3323ed9bb06f
Aidan Balasch Pérez <50273055+AidanBalap@users.noreply.github.com>
2024-03-12 16:48:17 +0100

    [fix] Time ago error

--- utils/dateToTimeAgo.ts
+++ utils/dateToTimeAgo.ts
@@ -1,9 +1,9 @@
 import TimeAgo from 'javascript-time-ago'
 import es from 'javascript-time-ago/locale/es'
 
-export default function (date: Date): String {
-    TimeAgo.addDefaultLocale(es)
-    const timeAgo = new TimeAgo('es-ES')
+TimeAgo.addDefaultLocale(es)
+const timeAgo = new TimeAgo('es-ES')
 
+export default function (date: Date): String {
     return timeAgo.format(date)
 }
